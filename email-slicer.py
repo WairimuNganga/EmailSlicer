1314b0478bba93776e9d70b2b4fa99f9fb8063f8
WairimuNganga <wairimuznganga@gmail.com>
2022-04-13 15:48:03 +0300

    changes made on idenfiers

--- email-slicer.py
+++ email-slicer.py
@@ -2,6 +2,6 @@ Email_id = input("Enter your email id").strip()
 
 username = Email_id[:Email_id.index('@')]
 
-domain = Email_id[Email_id.index('@')+1:]
+Domain = Email_id[Email_id.index('@')+1:]
 
-print(f"Your username {username} and your domain name is {domain}")
+print(f"Your username {username} and your domain name is {Domain}")
